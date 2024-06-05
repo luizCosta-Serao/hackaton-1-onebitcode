4bb8b3d17dbd7aa18c7ea52de9a4fbf673ffa14d
Luiz Serão <luiz.serao@hotmail.com>
2024-06-05 08:25:46 -0300

    artistas

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroHome from '../components/HeroHome/HeroHome'
 import Sobre from '../components/Sobre/Sobre'
+import Artistas from '../components/Artistas/Artistas'
 
 const Home = () => {
   return (
     <React.Fragment>
       <HeroHome />
       <Sobre />
+      <Artistas />
     </React.Fragment>
   )
 }
